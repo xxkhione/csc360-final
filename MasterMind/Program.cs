13efd016aad6df83760f31dc09e72614535a3695
Damia <dsargent@student.neumont.edu>
2026-05-30 18:54:07 -0600

    Implemented the Facade pattern

--- MasterMind/Program.cs
+++ MasterMind/Program.cs
@@ -24,11 +24,10 @@ namespace MasterMind
 
         static void Main(string[] args)
         {
-            List<Attempt> allAttempts = new List<Attempt>();
             List<string> difficulties = new List<string>() { "Easy", "Medium", "Hard" };
 
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("Welcome to Mastermind!");
+            Console.WriteLine("Welcome to MasterMind!");
             Console.ResetColor();
 
             Console.ForegroundColor = ConsoleColor.Cyan;
@@ -36,73 +35,8 @@ namespace MasterMind
             int maxTurns = MMLib.GetConsoleInt("How many turns would you like? (Max is 25 turns) ", 1, 25);
             Console.ResetColor();
 
-            CodeFactory factory = null;
-            switch(difficulty)
-            {
-                case 1:
-                    factory = new EasyCodeFactory();
-                    break;
-                case 2:
-                    factory = new MediumCodeFactory();
-                    break;
-                case 3:
-                    factory = new HardCodeFactory();
-                    break;
-            }
-
-            IAnswerGenerator generator = factory.CreateGenerator();
-            List<int> answer = generator.GenerateAnswer();
-            int maxPegs = answer.Count;
-
-            IGuessEvaluationStrategy guessEvaluationStrategy = new StandardGuessEvaluationStrategy();
-            MasterMindEngine engine = new MasterMindEngine(guessEvaluationStrategy);
-
-            //show cheat? 
-            //MMLib.Cheat(answer, pegList);
-
-            bool gameWon = false;
-            do
-            {
-                Attempt userAttempt = GetAttemptFromUser(maxPegs, allAttempts, maxTurns);
-                engine.EvaluateAttempt(answer, userAttempt);
-                allAttempts.Add(userAttempt);
-                gameWon = userAttempt.CorrectAnswerCount == maxPegs;
-                maxTurns--;
-            } while(!gameWon && maxTurns != 0);
-
-            if(gameWon)
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Congratulations, you have won the game!");
-                Console.ResetColor();
-            } 
-            else if(maxTurns == 0)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("You lose, you have ran out of turns.");
-                Console.ResetColor();
-            }
-            MMLib.ShowAnswer(answer, pegList, "x");
-        }
-
-        static Attempt GetAttemptFromUser(int maxPegs, List<Attempt> allAttempts, int maxTurns)
-        {
-            Attempt userAttempt = new Attempt();
-            List<string> colorOptions = MMLib.GetColorOptions(maxPegs, pegList);
-            for(int i = 0; i < maxPegs; i++)
-            {
-                Console.Clear();
-                Console.WriteLine(maxTurns);
-                MMLib.ShowAttempts(allAttempts, pegList, "x");
-                MMLib.ShowChosenPegs(userAttempt, pegList);
-                int colorSelection = MMLib.GetConsoleMenu(colorOptions) - 1;
-                userAttempt.AttemptList.Add(colorSelection);
-            }
-            Console.Clear();
-            Console.WriteLine(maxTurns);
-            MMLib.ShowAttempts(allAttempts, pegList, "x");
-            MMLib.ShowChosenPegs(userAttempt, pegList);
-            return userAttempt;
+            MasterMindGameFacade game = new MasterMindGameFacade(pegList, difficulty, maxTurns);
+            game.RunGame();
         }
     }
 }
